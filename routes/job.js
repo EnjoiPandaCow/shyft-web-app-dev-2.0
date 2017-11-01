4328421d876a4de9d72190c60cf692141135eeb0
CJ O'Sullivan <me@cjosullivan.net>
2017-11-01 13:54:35 +0000

    Completed Mongo Integration

--- routes/job.js
+++ routes/job.js
@@ -32,32 +32,6 @@ router.findOne = function(req,res) {
   });
 };
 
-/*
-router.addJob = function(req, res) {
-
-    var id = Math.floor((Math.random()*100000 + 1));
-    var currentSize = jobs.length;
-    jobs.push({
-        "id": id,
-        "title" : req.body.title,
-        "desc" : req.body.desc,
-        "size" : req.body.size,
-        "cLoc" : req.body.cLoc,
-        "dLoc" : req.body.dLoc,
-        "dTime" : req.body.dTime,
-        "price" : req.body.price,
-        "photos" : [
-            req.body.photos
-        ]});
-
-    if((currentSize + 1) === jobs.length)
-        res.json({ message: 'Job Added!'});
-    else
-        res.json({ message: 'Job Not Added!'});
-
-};
-*/
-
 router.addJob = function (req,res) {
     var job = new Job();
 
@@ -82,40 +56,23 @@ router.addJob = function (req,res) {
     });
 };
 
-router.updateJob = function(req,res) {
-
-    var job = getByValue(jobs, req.params.id);
-    var oldTitle = job.title;
-    var newTitle = req.body.title;
-
-    job.title = newTitle;
-
-    if (oldTitle !== newTitle)
-        res.json({message : 'Title Updated'});
-    else
-        res.json({message : 'Title not Updated '});
+router.updateJob = function (req,res) {
+  Job.findByIdAndUpdate(req.params.id, req.body, {new:true}, function(err, doc) {
+     if(err)
+         return res.json(err);
+     else
+         return res.json(doc);
+  });
 };
 
-router.deleteJob = function(req, res){
+router.deleteJob = function(req, res) {
 
-    var job = getByValue(jobs, req.params.id);
-    var index = jobs.indexOf(job);
-
-    var currentSize = jobs.length;
-    jobs.splice(index, 1);
-
-    if((currentSize - 1) === jobs.length)
-        res.json({message:'Job Deleted!'});
-    else
-        res.json({message:'Job Not Deleted!'});
+    Job.findByIdAndRemove(req.params.id, function(err) {
+        if(err)
+            res.send(err);
+        else
+            res.json({message: 'Donation Deleted!'});
+    });
 };
-
-
-function getByValue(arr, id) {
-
-    var result = arr.filter(function(o){return o.id === id;});
-    return result ? result[0] : null;
-
-}
 
 module.exports = router;
